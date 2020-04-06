dee61b108439f54227d962631d0a2765a49ddd5e
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-06 19:45:29 -0400

    [Anmol Singh]Complaint Filling System - Recycler View Integrated(with Intent)

--- app/src/main/java/com/lambton/complaint_filing_system/RecyclerViewActivity.java
+++ app/src/main/java/com/lambton/complaint_filing_system/RecyclerViewActivity.java
@@ -1,8 +1,10 @@
 package com.lambton.complaint_filing_system;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
@@ -27,8 +29,14 @@ public class RecyclerViewActivity extends AppCompatActivity {
         countryListView = findViewById(R.id.rvCountryList);
         populateCountries();
         countriesAdapter = new CountriesAdapter(countryList);
-        RecyclerView.LayoutManager thisLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,
+//        RecyclerView.LayoutManager thisLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,
+//                false);
+        RecyclerView.LayoutManager thisLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,
                 false);
+
+        GridLayoutManager mgridView = new GridLayoutManager(this,2);
+
+//        StaggeredGridLayoutManager stagGrid = new StaggeredGridLayoutManager(this,2);
         countryListView.setLayoutManager(thisLayoutManager);
         countryListView.setAdapter(countriesAdapter);
 
@@ -53,38 +61,6 @@ public class RecyclerViewActivity extends AppCompatActivity {
         countryList.add(new Country(13,"India",R.drawable.bb,"India is my home country"));
         countryList.add(new Country(14,"India",R.drawable.bd,"India is my home country"));
         countryList.add(new Country(15,"India",R.drawable.be,"India is my home country"));
-        countryList.add(new Country(1,"India",R.drawable.ag,"India is my home country"));
-        countryList.add(new Country(2,"India",R.drawable.ad,"India is my home country"));
-        countryList.add(new Country(3,"India",R.drawable.ae,"India is my home country"));
-        countryList.add(new Country(4,"India",R.drawable.af,"India is my home country"));
-        countryList.add(new Country(5,"India",R.drawable.al,"India is my home country"));
 
-        countryList.add(new Country(6,"India",R.drawable.am,"India is my home country"));
-        countryList.add(new Country(7,"India",R.drawable.ao,"India is my home country"));
-        countryList.add(new Country(8,"India",R.drawable.ar,"India is my home country"));
-        countryList.add(new Country(9,"India",R.drawable.at,"India is my home country"));
-        countryList.add(new Country(10,"Australia",R.drawable.au,"India is my home country"));
-
-        countryList.add(new Country(11,"India",R.drawable.az,"India is my home country"));
-        countryList.add(new Country(12,"India",R.drawable.ba,"India is my home country"));
-        countryList.add(new Country(13,"India",R.drawable.bb,"India is my home country"));
-        countryList.add(new Country(14,"India",R.drawable.bd,"India is my home country"));
-        countryList.add(new Country(15,"India",R.drawable.be,"India is my home country")); countryList.add(new Country(1,"India",R.drawable.ag,"India is my home country"));
-        countryList.add(new Country(2,"India",R.drawable.ad,"India is my home country"));
-        countryList.add(new Country(3,"India",R.drawable.ae,"India is my home country"));
-        countryList.add(new Country(4,"India",R.drawable.af,"India is my home country"));
-        countryList.add(new Country(5,"India",R.drawable.al,"India is my home country"));
-
-        countryList.add(new Country(6,"India",R.drawable.am,"India is my home country"));
-        countryList.add(new Country(7,"India",R.drawable.ao,"India is my home country"));
-        countryList.add(new Country(8,"India",R.drawable.ar,"India is my home country"));
-        countryList.add(new Country(9,"India",R.drawable.at,"India is my home country"));
-        countryList.add(new Country(10,"Australia",R.drawable.au,"India is my home country"));
-
-        countryList.add(new Country(11,"India",R.drawable.az,"India is my home country"));
-        countryList.add(new Country(12,"India",R.drawable.ba,"India is my home country"));
-        countryList.add(new Country(13,"India",R.drawable.bb,"India is my home country"));
-        countryList.add(new Country(14,"India",R.drawable.bd,"India is my home country"));
-        countryList.add(new Country(15,"India",R.drawable.be,"India is my home country"));
     }
 }
--- app/src/main/java/com/lambton/complaint_filing_system/adapters/CountriesAdapter.java
+++ app/src/main/java/com/lambton/complaint_filing_system/adapters/CountriesAdapter.java
@@ -1,14 +1,18 @@
 package com.lambton.complaint_filing_system.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.lambton.complaint_filing_system.ComplaintEntryActivity;
 import com.lambton.complaint_filing_system.R;
 import com.lambton.complaint_filing_system.models.Country;
 
@@ -34,10 +38,19 @@ public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.Coun
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CountryViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final CountryViewHolder holder, final int position) {
         Country countryObject = this.countryArrayList.get(position);
         holder.countryNameText.setText(countryObject.getCountryName());
         holder.countryFlagImage.setImageResource(countryObject.getCountryFlag());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Country name = countryArrayList.get(position);
+                Toast.makeText(holder.itemView.getContext(), "Name: "+ name.getCountryName(), Toast.LENGTH_SHORT).show();
+                Intent mIntent = new Intent(holder.itemView.getContext(), ComplaintEntryActivity.class);
+                holder.itemView.getContext().startActivity(mIntent);
+            }
+        });
     }
 
     @Override
